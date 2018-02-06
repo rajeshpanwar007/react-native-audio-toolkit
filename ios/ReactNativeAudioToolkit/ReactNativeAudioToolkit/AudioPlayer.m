0c14e42dc8786a0018e1cd4b650c684163e42b3e
rajeshpanwar007 <rajeshpanwar007@gmail.com>
2018-02-06 12:48:32 +0530

    Update AudioPlayer.m

--- ios/ReactNativeAudioToolkit/ReactNativeAudioToolkit/AudioPlayer.m
+++ ios/ReactNativeAudioToolkit/ReactNativeAudioToolkit/AudioPlayer.m
@@ -151,26 +151,51 @@ RCT_EXPORT_METHOD(prepare:(nonnull NSNumber*)playerId
         callback(@[dict]);
         return;
     }
-    
+
     // Prepare the player
-    // Wait until player is ready
-    while (player.status == AVPlayerStatusUnknown) {
-        [NSThread sleepForTimeInterval:0.01f];
-    }
-    
-    // Callback when ready / failed
-    if (player.status == AVPlayerStatusReadyToPlay) {
-        callback(@[[NSNull null]]);
-    } else {
-        NSDictionary* dict = [Helpers errObjWithCode:@"preparefail"
-                                         withMessage:[NSString stringWithFormat:@"Preparing player failed"]];
-        
-        if (player.autoDestroy) {
-            [self destroyPlayerWithId:playerId];
+        // Wait until player is ready
+        while (player.status == AVPlayerStatusUnknown) {
+            [NSThread sleepForTimeInterval:0.01f];
         }
-        
-        callback(@[dict]);
-    }
+
+    	//make sure loadedTimeRanges is not null
+    	while (player.currentItem.loadedTimeRanges.firstObject == nil){
+    	  [NSThread sleepForTimeInterval:0.01f];
+    	}
+
+        float version = [[[UIDevice currentDevice] systemVersion] floatValue];
+
+        //wait until 10 seconds are buffered then play
+        if (version >= 10.0) {
+            player.currentItem.preferredForwardBufferDuration = 500;
+        }
+        Float64 durationSeconds = 0;
+        while (durationSeconds < 10){
+            NSValue *val = player.currentItem.loadedTimeRanges.firstObject;
+            CMTimeRange timeRange;
+            [val getValue:&timeRange];
+            durationSeconds = CMTimeGetSeconds(timeRange.duration);
+            [NSThread sleepForTimeInterval:0.01f];
+        }
+
+        // Callback when ready / failed
+        if (player.status == AVPlayerStatusReadyToPlay) {
+            if (version >= 10.0) {
+                player.automaticallyWaitsToMinimizeStalling = false;
+            }
+            callback(@[[NSNull null]]);
+        } else {
+            NSDictionary* dict = [Helpers errObjWithCode:@"preparefail"
+                                             withMessage:[NSString stringWithFormat:@"Preparing player failed"]];
+
+            if (player.autoDestroy) {
+                [self destroyPlayerWithId:playerId];
+            }
+
+            callback(@[dict]);
+        }
+
+
 }
 
 RCT_EXPORT_METHOD(destroy:(nonnull NSNumber*)playerId withCallback:(RCTResponseSenderBlock)callback) {
